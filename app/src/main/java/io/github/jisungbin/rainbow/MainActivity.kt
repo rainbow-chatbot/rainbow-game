9ef606884cebeb6036ef402d748045fd54d1ca4e
jisungbin <ji@sungb.in>
2021-07-22 20:00:04 +0900

    :rainbow: First commit :rainbow:

---
+++ app/src/main/java/io/github/jisungbin/rainbow/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.jisungbin.rainbow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.github.jisungbin.rainbow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
